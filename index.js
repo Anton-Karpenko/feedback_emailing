29557b63b5e1b11b5f5d0e9dde4c048a95fd6c65
Anton-Karpenko <anton.karpenko97@gmail.com>
2017-08-15 17:19:33 +0300

    Finished auth flow

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require('express');
+const keys = require('./config/keys');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+require('./models/User');
 require('./services/passport');
 
 const app = express();
+
+mongoose.connect(keys.mongoURI, {
+  useMongoClient: true
+});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,17 @@
 const passport = require('passport');
 const GoogleStategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => done(null, user));
+});
 
 passport.use(
   new GoogleStategy(
@@ -10,8 +21,15 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('access token', accessToken);
-      console.log('profile:', profile);
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
